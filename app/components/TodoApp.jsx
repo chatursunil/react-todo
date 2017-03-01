cd283d841337920b7413584c33157a6583a2578d
Sunil Chaturvedi <Sunil Chaturvedi>
2017-03-01 09:32:43 -0500

    Add Search Component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,13 @@
 const React = require('react');
 const TodoList = require('TodoList');
 const AddTodo = require('AddTodo');
+const TodoSearch = require('TodoSearch');
 
 const TodoApp = React.createClass({
     getInitialState: function() {
         return({
+            showCompleted: false,
+            searchText: '',
             todos: [
                 {
                     id: 1,
@@ -28,10 +31,17 @@ const TodoApp = React.createClass({
     handleAddTodo: function(text) {
         alert('AddTodo: ' + text);
     },
+    handleSearch: function(showCompleted, searchText) {
+        this.setState({
+           showCompleted: showCompleted,
+           searchText: searchText.toLowerCase() 
+        });
+    },
     render: function() {
         let {todos} = this.state;
         return (
             <div>
+                <TodoSearch onSearch={this.handleSearch}></TodoSearch>
                 <TodoList todos={todos}></TodoList>
                 <AddTodo onAddTodo={this.handleAddTodo}></AddTodo>
             </div>
---
+++ app/components/TodoSearch.jsx
@@ -0,0 +1,27 @@
+const React = require('react');
+
+const TodoSearch = React.createClass({
+    handleSearch: function() {
+        let showCompleted = this.refs.showCompleted.checked;
+        let searchText = this.refs.searchText.value;
+
+        this.props.onSearch(showCompleted, searchText);
+    },
+    render: function() {
+        return (
+            <div>
+                <div>
+                    <input type="search" ref="searchText" placeholder="Search todos" onChange={this.handleSearch}/>
+                </div>
+                <div>
+                    <label htmlFor="showCompleted">
+                        <input type="checkbox" id="showCompleted" ref="showCompleted" onChange={this.handleSearch}/>
+                        Show Completed
+                    </label>
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = TodoSearch;
