5dfaf8a663330002e282ad572156b5fe0b2141cd
Sunil Chaturvedi <Sunil Chaturvedi>
2017-03-01 14:45:48 -0500

    Add localstorage support

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,37 +1,21 @@
 const React = require('react');
+const uuid = require('node-uuid');
+
 const TodoList = require('TodoList');
 const AddTodo = require('AddTodo');
 const TodoSearch = require('TodoSearch');
-const uuid = require('node-uuid');
+const TodoAPI = require('TodoAPI');
 
 const TodoApp = React.createClass({
     getInitialState: function() {
         return({
             showCompleted: false,
             searchText: '',
-            todos: [
-                {
-                    id: uuid(),
-                    text: 'Walk the dog',
-                    completed: false
-                },
-                {
-                    id: uuid(),
-                    text: 'Clean the yard',
-                    completed: true
-                },
-                {
-                    id: uuid(),
-                    text: 'Go running',
-                    completed: true
-                },
-                {
-                    id: uuid(),
-                    text: 'Have shower',
-                    completed: false
-                }
-            ]
+            todos: TodoAPI.getTodos()
         });
+    },
+    componentDidUpdate: function() {
+        TodoAPI.setTodos(this.state.todos);
     },
     handleAddTodo: function(text) {
         this.setState({
