17db1541e75b7d62710b26279f4975e75e41ddfc
Sunil Chaturvedi <Sunil Chaturvedi>
2017-03-08 10:39:43 -0500

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,16 @@ const ReactDOM = require('react-dom');
 const {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 const TodoApp = require('TodoApp');
+const actions = require('actions');
+const store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New State:', store.getState());
+})
+
+store.dispatch(actions.addTodo('Feed the birds'));
+store.dispatch(actions.setSearchText('bird'));
+store.dispatch(actions.toggleShowCompleted());
 
 // Load foundation
 $(document).foundation();
